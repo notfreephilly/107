fedc69569e71402394d8a5d26a2189aa1a0f8c03
philip yoder <notphilyoder@philips-MacBook-Pro.local>
2024-03-05 20:58:23 -0800

    python test script

---
+++ test.py
@@ -0,0 +1,34 @@
+
+
+# assign a variablee
+fork = "utencil"
+print(fork)
+
+first_name = 'philip'
+last_name = 'yoder'
+age = 113
+height = '5ft 9in'
+weight = '168lbs'
+is_single = True
+is_admin = False
+
+# if/else statement
+if age < 100:
+    print('baby')
+elif age >= 100:
+    print('you are so old...')
+else:
+    print('do you exist??')
+
+
+# functions
+
+def say_hello():
+    print('hi i guess')
+
+def say_goodbye(name):
+    print('bye' + name)
+
+say_hello()
+say_goodbye(' felicia')
+
