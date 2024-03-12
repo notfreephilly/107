1a85625512c59324135cebfe7da0b07041198524
philip yoder <notphilyoder@philips-MacBook-Pro.local>
2024-03-11 17:10:08 -0700

    installed thunderbolt, aded new landing pages

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request
+import json
 
 app = Flask(__name__) #__name__ this  is using the  name of the folder
 
@@ -6,6 +7,45 @@ app = Flask(__name__) #__name__ this  is using the  name of the folder
 def home():
     return "Hello from Flask"
 
+@app.get('/testing')
+def test():
+    return "hello from another page"
+
+@app.get('/about')
+def about():
+    me = {'name': 'Philip'}
+    return json.dumps(me)
+
+@app.get('/blog')
+def blog():
+    return "this is a blog"
+
+@app.get('/version')
+def version():
+    version =  {'name': 'megatron', 'version': '2.1', 'build': 'm3g@tR0NNN'}
+    return json.dumps(version)
+
+
+# read and write to server
+products = []
+@app.get('/api/products')
+def get_products():
+    return json.dumps(products)
+
+@app.post('/api/products')
+def save_products():
+    # should save a new product
+    product = request.get_json()
+    print (product)
+    # mock the save
+    products.append(product)
+    return json.dumps(product)
+
+
+    
+
 
 app.run(debug = True) # apply the changes on the code, live
 
+# API / Endpoints
+# transform json / convert json into an object again
